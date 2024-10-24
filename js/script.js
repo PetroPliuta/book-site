9cb0ad09bff0c2df25fc05d3ff5d896379bac895
Petro Pliuta <pet.pluta@gmail.com>
2024-10-24 23:13:02 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const stone = document.querySelector('.stone');
 
-    // Wait for 3 seconds, then slowly change the stone image to the pointers image
     setTimeout(() => {
-        stone.style.opacity = '0'; // Start fading out the stone
-        setTimeout(() => {
-            stone.style.backgroundImage = "url('./img/pointers.jpg')"; // Change to the pointers image
-            stone.style.opacity = '1'; // Fade in the new image
-        }, 1500); // Wait for the fade out to complete (1.5s)
-    }, 3000); // Wait 3 seconds after page load
+        stone.style.backgroundImage = "url('./img/pointers.jpg')"; // Change to the pointers image
+    }, 1500); // Wait for the fade out to complete (1.5s)
 });
