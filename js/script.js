05d9330ea1332d13cdb2a3cf1309b1afd1c50b35
Petro Pliuta <pet.pluta@gmail.com>
2024-10-25 23:00:56 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const stone = document.querySelector('.stone');
+// $(window).on("load", function () {
+$(document).ready(function () {
+    // const stone = $('.stone')[0];
+    const drevne = $('#drevne')[0];
+    const staro = $('#staro')[0];
 
-    setTimeout(() => {
-        stone.style.backgroundImage = "url('./img/pointers.jpg')"; // Change to the pointers image
-    }, 1500); // Wait for the fade out to complete (1.5s)
+    $('#start-btn').on('click', function () {
+        $(this).fadeOut(); // Hide the button with a fade-out effect
+        $('#overlay').fadeOut(); // Remove the dim overlay
+        $('.container').css('opacity', '1'); // Restore full opacity to main content
+
+        drevne.play(); // Play the sound
+
+        // Example: Change background image after 3 seconds
+        setTimeout(function () {
+
+            $('.stone').fadeOut(1000, function () { // Fade out current background
+                $(this).css('background-image', `url('img/three_pointers.jpg')`); // Change the background image
+                $(this).fadeIn(1000); // Fade in new background
+
+                staro.play();
+            });
+        }, 3000); // Delay for demonstration
+    });
 });
